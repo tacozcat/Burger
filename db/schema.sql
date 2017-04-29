aa1ba5ced071d72291fcf2cc415c4bcfe47a1d6a
Emily Adams <tacozcat@Emilys-MacBook-Air.local>
2017-04-28 20:00:46 -0500

    Database updates, need to fix handlebars

---
+++ db/schema.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE burgers_db;
+USE burgers_db;
+CREATE TABLE burgers(
+    id INTEGER(11) AUTO_INCREMENT NOT NULL,
+    burger_name VARCHAR(100) NULL,
+    devoured BOOLEAN DEFAULT FALSE,
+    date TIMESTAMP 
+);
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+SELECT * FROM burgers_db.burgers;
+USE burgers_db;
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Double Trouble", FALSE),("Buffalo Hot Times", TRUE),("Veggie Heaven", FALSE);
